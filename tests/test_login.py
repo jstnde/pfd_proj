5ae7c1c233e597e7db059d25f0e2f05fccfa3121
jstnde <nahcnitsuj@gmail.com>
2022-11-12 13:46:41 +0800

    updated login page tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,17 +8,53 @@ class LoginTest(LoginPage):
 
         self.open_page()
 
-    # Test Case 1 : Login
+    # Test Case 1 : Unsuccessful Login
     def test_login(self):
-        self.send_keys(LoginPage.input_field_username, LoginPage.input_value_username)
-        self.send_keys(LoginPage.input_field_password, LoginPage.input_value_password)
+        self.type(LoginPage.input_field_username, LoginPage.input_value_username)
+        self.type(LoginPage.input_field_password, LoginPage.input_value_password)
+
+        # TODO: print log here
+
+        self.scroll_to(LoginPage.input_field_password)
+        self.save_screenshot("form_field",
+                             LoginPage.custom_screenshot_dir +
+                             "/test_login")
 
         self.click(LoginPage.submit_btn)
 
         self.assert_text("Index", "h1")
 
-    def test_failed_login(self):
-        self.send_keys(LoginPage.input_field_username, LoginPage.input_field_username)
-        self.send_keys(LoginPage.input_field_password, LoginPage.input_value_invalid_password)
+        # TODO: print log here
+
+        self.save_screenshot("products_page",
+                             LoginPage.custom_screenshot_dir +
+                             "/test_login")
+
+        # TODO:
+        #  exception handling
+
+    # Test Case 2 : Unsuccessful Login
+    def test_login_error_msg(self):
+        self.type(LoginPage.input_field_username, LoginPage.input_value_invalid_username)
+        self.type(LoginPage.input_field_password, LoginPage.input_value_password)
+
+        # TODO: print log here
+
+        self.scroll_to(LoginPage.input_field_password)
+        self.save_screenshot("form_field",
+                             LoginPage.custom_screenshot_dir +
+                             "/test_login_error_msg")
 
         self.click(LoginPage.submit_btn)
+
+        self.assert_true(self.is_text_visible(LoginPage.error_text, LoginPage.error_span))
+
+        # TODO: print log here
+
+        self.scroll_to(LoginPage.error_span)
+        self.save_screenshot("error_msg",
+                             LoginPage.custom_screenshot_dir +
+                             "/test_login_error_msg")
+
+        # TODO:
+        #  exception handling
