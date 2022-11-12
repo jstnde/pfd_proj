374b81bd8774ff92a548d4da913a9ac49ecfa400
jstnde <nahcnitsuj@gmail.com>
2022-11-12 14:25:07 +0800

    updated create product page test cases

--- tests/test_create_product.py
+++ tests/test_create_product.py
@@ -48,3 +48,66 @@ class CreateProductTest(CreateProductPage):
 
         # TODO:
         #  exception handling
+
+    # Test Case 4 : Image Upload Validation
+    def test_image_validation(self):
+        self.type(CreateProductPage.input_field_title, CreateProductPage.input_value_title)
+
+        # TODO: print log here
+
+        self.type(CreateProductPage.input_field_price, CreateProductPage.input_value_price)
+
+        # TODO: print log here
+
+        self.scroll_to(CreateProductPage.input_field_price)
+        self.save_screenshot("empty_image_create_product_page",
+                             CreateProductPage.custom_screenshot_dir +
+                             "/test_image_validation")
+
+        self.click(CreateProductPage.submit_btn)
+
+        # TODO:
+        #  add assertion
+        #  exception handling
+
+    # Test Case 5 : Title Field Validation
+    def test_title_validation(self):
+        self.choose_file(CreateProductPage.input_field_file, CreateProductPage.product_file_path)
+
+        # TODO: print log here
+
+        self.type(CreateProductPage.input_field_price, CreateProductPage.input_value_price)
+
+        # TODO: print log here
+
+        self.scroll_to(CreateProductPage.input_field_price)
+        self.save_screenshot("empty_title_create_product_page",
+                             CreateProductPage.custom_screenshot_dir +
+                             "/test_title_validation")
+
+        self.click(CreateProductPage.submit_btn)
+
+        # TODO:
+        #  add assertion
+        #  exception handling
+
+    # Test Case 6 : Price Field Validation
+    def test_price_validation(self):
+        self.choose_file(CreateProductPage.input_field_file, CreateProductPage.product_file_path)
+
+        # TODO: print log here
+
+        self.type(CreateProductPage.input_field_title, CreateProductPage.input_value_title)
+
+        # TODO: print log here
+
+        self.scroll_to(CreateProductPage.input_field_price)
+        self.save_screenshot("empty_price_create_product_page",
+                             CreateProductPage.custom_screenshot_dir +
+                             "/test_price_validation")
+
+        self.click(CreateProductPage.submit_btn)
+
+        # TODO:
+        #  add assertion
+        #  exception handling
