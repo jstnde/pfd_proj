d3843ffad449f802375e0eccc72e0fd8c65df53d
jstnde <nahcnitsuj@gmail.com>
2022-11-15 11:21:37 +0800

    update login test case

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,5 @@
+from seleniumbase.common import exceptions
+
 from page_objects.login_page import LoginPage
 
 
@@ -13,8 +15,8 @@ class LoginTest(LoginPage):
         self.type(LoginPage.input_field_username, LoginPage.input_value_username)
         self.type(LoginPage.input_field_password, LoginPage.input_value_password)
 
-        print("Input right testing username : 'ProductManager'")
-        print("Input right testing password : 'passProduct'")
+        print("Input right testing username : '" + LoginPage.input_value_username + "'")
+        print("Input right testing password : '" + LoginPage.input_value_password + "'")
 
         self.scroll_to(LoginPage.input_field_password)
         self.save_screenshot("form_field",
@@ -23,25 +25,23 @@ class LoginTest(LoginPage):
 
         self.click(LoginPage.submit_btn)
 
-        self.assert_text("Index", "h1")
-
         print("Click Submit button")
-        print("Successful sign in")
 
         self.save_screenshot("products_page",
                              LoginPage.custom_screenshot_dir +
                              "/test_login")
 
-        # TODO:
-        #  exception handling
+        self.assert_text("Index", "h1")
+
+        print("Successful sign in")
 
     # Test Case 2 : Unsuccessful Login
     def test_login_error_msg(self):
         self.type(LoginPage.input_field_username, LoginPage.input_value_invalid_username)
         self.type(LoginPage.input_field_password, LoginPage.input_value_password)
 
-        print("Input wrong testing username : 'ProductManager123'")
-        print("Input right testing password : 'passProduct'")
+        print("Input wrong testing username : '" + LoginPage.input_value_invalid_username + "'")
+        print("Input right testing password : '" + LoginPage.input_value_password + "'")
 
         self.scroll_to(LoginPage.input_field_password)
         self.save_screenshot("form_field",
@@ -50,15 +50,13 @@ class LoginTest(LoginPage):
 
         self.click(LoginPage.submit_btn)
 
-        self.assert_true(self.is_text_visible(LoginPage.error_text, LoginPage.error_span))
-
         print("Click Submit button")
-        print("Error message 'User does not exist' appears")
 
         self.scroll_to(LoginPage.error_span)
         self.save_screenshot("error_msg",
                              LoginPage.custom_screenshot_dir +
                              "/test_login_error_msg")
 
-        # TODO:
-        #  exception handling
+        self.assert_true(self.is_text_visible(LoginPage.error_text, LoginPage.error_span))
+
+        print("Error message '" + LoginPage.error_text + "' appears")
